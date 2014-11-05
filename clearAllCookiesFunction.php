2e655e2131cc3849139bffc1f7a3a9eb7c4281b9
LostNerdUK <peter@lostnerd.uk>
2014-11-05 16:21:28 +0000

    Add clearCookies() script
    A simple script that unsets all cookies stored in $_COOKIE and also the
    cookie stored on the users computer.

---
+++ clearAllCookiesFunction.php
@@ -0,0 +1,12 @@
+<?php
+
+//A simple script that unsets all cookies stored in $_COOKIE and also the cookie stored on the users computer.
+
+function clearCookies() {
+	foreach($_COOKIE as $one => $two) {
+		unset($_COOKIE[$one]);
+		setcookie($one, "", time()-3600);
+	}
+	return true;
+}
+?>
