d676a5ac7ddc0b5dfedd2a3d02ee624c0d012da9
Peter Bishop <LostNerdUK@users.noreply.github.com>
2015-07-14 10:58:39 +0100

    Update fixSerializedArrayOffsetError.php

--- fixSerializedArrayOffsetError.php
+++ fixSerializedArrayOffsetError.php
@@ -1,4 +1,5 @@
 <?php
-$array = preg_replace('!s:(\d+):"(.*?)";!e', "'s:'.strlen('$2').':\"$2\";'", $row['array']);
-$array = unserialize($array);
+$brokenArray = "Serialized array here";
+$fixedArray = preg_replace('!s:(\d+):"(.*?)";!e', "'s:'.strlen('$2').':\"$2\";'", $brokenArray);
+$fixedArray = unserialize($array);
 ?>
